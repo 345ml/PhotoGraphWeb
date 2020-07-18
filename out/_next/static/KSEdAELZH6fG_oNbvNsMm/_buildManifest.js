9a1b5b7ef0afc31792d609e52ec3532912891c72
Keisuke Horota <keisuke.horota@gmail.com>
2020-07-18 18:14:11 +0900

    Deploy Next.js to gh-pages

---
+++ out/_next/static/KSEdAELZH6fG_oNbvNsMm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002Fa9a7754c.0ced58fedfbe6a37ccf1.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
